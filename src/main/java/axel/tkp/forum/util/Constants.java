da5c58581e2c4a338369540914cae518457b4c87
axel <agile@null.net>
2016-03-13 12:32:33 +0200

    .

--- src/main/java/axel/tkp/forum/util/Constants.java
+++ src/main/java/axel/tkp/forum/util/Constants.java
@@ -17,8 +17,5 @@ public class Constants {
     public static final String DATABASE_NAME = "forum.db";
     
     public static final String MESSAGE_404 = "Resource not found. <b>Shoo!</b>";
-    
-    public static final String POSTGRE_DB = "postgres://pnjpzjiukhbger:F-oGcnPKPFrWK4pjaHgeVn3ssZ@ec2-107-21-10" +
-        "1-67.compute-1.amazonaws.com:5432/d3fmlht19843uu";
 
 }
